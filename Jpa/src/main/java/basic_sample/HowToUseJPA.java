1adb54c35cc729569fa3f9afd7dc1a24ebb81d8c
Onji Kim <xodks672@gmail.com>
2023-06-22 17:30:04 +0900

    [ADD] JPA project base

---
+++ Jpa/src/main/java/basic_sample/HowToUseJPA.java
@@ -0,0 +1,36 @@
+package basic_sample;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class HowToUseJPA {
+    public static void main(String[] args) {
+        //엔티티 메니저 팩토리 생성
+        //팩토리는 생성 비용이 크고, 쓰레드간 공유가 가능하므로 애플리케이션 전체에서 딱 한번만 생성하고 공유해서 사용해야 한다.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2database");
+
+        //엔티티 메니저 생성
+        //엔티티 매니저는 생성비용이 적고 공유되어서는 안된다.
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션 획득
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin(); //트랜잭션 시작
+            logic(em); //비즈니스 로직 실행
+            tx.commit(); //트랜잭션 커밋
+        } catch (Exception e){
+            tx.rollback(); //롤백
+        } finally {
+            em.close(); //엔티티 메니저 종료
+        }
+        emf.close(); //엔티티 메니저 팩토리 종료
+    }
+
+    public static void logic(EntityManager entityManager){
+        //do something
+    }
+}

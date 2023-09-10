c9f58364280380fa4a5bb1a909b8435b26bde4f9
Onji Kim <xodks672@gmail.com>
2023-09-10 12:05:06 +0900

    reactor

---
+++ Reactor/src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Flux<String> keyboardInputFlux = Flux.create(sink -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                printWithThread("Type something (or 'exit' to quit): ");
+                String input = scanner.nextLine();
+
+                if ("exit".equalsIgnoreCase(input)) {
+                    sink.complete(); // Flux를 종료
+                    scanner.close();
+                    break;
+                }
+
+                sink.next(input); // 입력된 문자열을 Flux에 방출
+            }
+        }, FluxSink.OverflowStrategy.IGNORE); // 오버플로우 시 무시
+        CountDownLatch latch = new CountDownLatch(1);
+        keyboardInputFlux
+                .doOnTerminate(latch::countDown)
+                .subscribeOn(Schedulers.single())
+                .parallel()
+                .runOn(Schedulers.boundedElastic())
+                .subscribe(
+                data -> printWithThread("Received: " + data),
+                error -> printWithThread("Error: " + error),
+                () -> printWithThread("Complete!")
+        );
+        Flux.interval(Duration.ofSeconds(3))
+                .take(3)
+                .subscribe(s -> printWithThread( s + "seconds elapsed"));
+        printWithThread("Waiting for terminal signal...");
+
+        latch.await();
+
+    }
+
+    public static void printWithThread(String data){
+        System.out.printf("\n[%s] %s\n",Thread.currentThread().getName(),data);
+    }
+}
